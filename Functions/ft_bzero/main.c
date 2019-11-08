ed3c88d7bd92ed70c9a41c4b5646383666c5893d
ccastill <ccastill@student.42madrid.com>
2019-11-08 23:57:45 +0100

    1

--- Functions/ft_bzero/main.c
+++ Functions/ft_bzero/main.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 14:37:52 by ccastill          #+#    #+#             */
-/*   Updated: 2019/11/08 14:41:13 by ccastill         ###   ########.fr       */
+/*   Updated: 2019/11/08 23:56:31 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void ft_bzero(void *s, size_t n);
 
 int	main(void)
 {
-	char str[25] = "Hola que tal?";
+	char str[25] = "PRueba?";
 	ft_bzero(str, 'p', 25);
 	printf("%s\n", str);
 	memset(str, 'p', 25);
