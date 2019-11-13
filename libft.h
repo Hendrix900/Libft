d0c5fcc1b0834c56e18612e85ada22b352636617
ccastill <Carlosc90@gmail.com>
2019-11-13 19:53:32 +0100

    1

--- libft.h
+++ libft.h
@@ -21,6 +21,11 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memset(void *str, int c, size_t n);
+void 	*ft_memmove(void *dest, const void *src, size_t n);
+void 	*ft_memcpy(void *dest, const void *src, size_t n);
+void 	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+int 	ft_memcmp(const void *s1, const void *s2, size_t n);
+void 	*ft_memchr(const void *s, int c, size_t n)
 size_t	ft_strlcpy(char *dest, char *src, unsigned int size);
 size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *str1, const char *str2, unsigned int n);
