f520bafae600bacf20e307cbecfa213987ccfe49
Carlos Castillo fernandez <ccastill@c3r2s2.42madrid.com>
2019-11-15 14:14:33 +0100

    1

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 14:06:46 by ccastill          #+#    #+#             */
-/*   Updated: 2019/11/15 13:53:09 by ccastill         ###   ########.fr       */
+/*   Updated: 2019/11/15 14:02:19 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	q = (unsigned char*)src;
 	p = (unsigned char*)dest;
 	l = 0;
+	if (dest == '\0' && src == '\0')
+		return (0);
 	while (n > 0)
 	{
 		p[l] = q[l];
