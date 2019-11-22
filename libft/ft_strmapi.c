faef06b5703f1608e40e71d0a438d42fb98f9645
ccastill <Carlosc90@gmail.com>
2019-11-22 16:47:03 +0100

    1

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
--- libft/libft.h
+++ libft/libft.h
@@ -40,6 +40,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, char *src, unsigned int size);
 size_t	ft_strlen(const char *str);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *str, int c);
