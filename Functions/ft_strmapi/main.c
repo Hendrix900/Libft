8514de2cd1d2f9d216c1690dc625743b8554fa1f
ccastill <Carlosc90@gmail.com>
2019-11-22 16:45:50 +0100

    1

--- Functions/ft_strmapi/main.c
+++ Functions/ft_strmapi/main.c
@@ -1,32 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/21 16:03:43 by ccastill          #+#    #+#             */
-/*   Updated: 2019/11/21 16:09:56 by ccastill         ###   ########.fr       */
+/*   Created: 2019/11/20 22:01:20 by ccastill          #+#    #+#             */
+/*   Updated: 2019/11/20 22:01:20 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char *f(unsigned int, char)
+
+int main() 
 {
-	char	*new;
-	
-	int	l;
-	
-	if (!(new = malloc(sizeof(char) * (ft_strlen(s) + 1 )
-		return (NULL);
-	while (s[l])
-	{	
-		new[l] = f(l, s[l]);
-		l++;
-	}
-	new[l] = '\0';
-	return (new);
+   char const *s= "Hola";
+   char *f= "Mundo";
+   char *result;
+   result = ft_strmapi(*s, *f);
+   printf("The string : %s\n", result);
+   return 0;
 }
