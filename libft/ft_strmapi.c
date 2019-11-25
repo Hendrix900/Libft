e7a666fd5e6a6ca04a19d1d3025b8d96dc85b758
Carlos Castillo fernandez <ccastill@c3r9s4.42madrid.com>
2019-11-25 13:00:43 +0100

    1

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,24 +6,24 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 16:03:43 by ccastill          #+#    #+#             */
-/*   Updated: 2019/11/21 16:09:56 by ccastill         ###   ########.fr       */
+/*   Updated: 2019/11/25 12:59:35 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*new;
-	
-	int	l;
-	
-	if (!(new = malloc(sizeof(char) * (ft_strlen(s) + 1 )
+	int		l;
+
+	l = 0;
+	if (!s)
+		return (NULL);
+	if (!(new = malloc(sizeof(char) * (ft_strlen(s) + 1))))
 		return (NULL);
 	while (s[l])
-	{	
+	{
 		new[l] = f(l, s[l]);
 		l++;
 	}
