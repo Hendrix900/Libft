b723aae40dc6f36bd94f2e656b41146b2c8787ba
ccastill <carlosc90@gmail.com>
2019-12-04 16:53:45 +0100

    1

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: carlos <carlos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 22:16:49 by ccastill          #+#    #+#             */
-/*   Updated: 2019/11/15 13:03:27 by ccastill         ###   ########.fr       */
+/*   Updated: 2019/12/04 16:49:50 by carlos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strrchr(const char *str, int c)
 {
-	char	*end;
+	const char	*end;
 
 	end = 0;
 	while (*str)
