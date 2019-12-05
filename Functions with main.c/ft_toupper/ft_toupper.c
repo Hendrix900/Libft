551e25ff3fc93714805e3142f270ed9322902314
Carlos Castillo fernandez <ccastill@c3r10s4.42madrid.com>
2019-12-05 17:30:59 +0100

    Final

--- Functions with main.c/ft_toupper/ft_toupper.c
+++ Functions with main.c/ft_toupper/ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 14:02:50 by ccastill          #+#    #+#             */
-/*   Updated: 2019/11/06 14:03:27 by ccastill         ###   ########.fr       */
+/*   Updated: 2019/12/05 17:30:43 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,4 @@ int	ft_toupper(int c)
 	}
 	else
 		return (c); //Si no hay minúsculas devuelve el valor que se ha pasado
-}
 }
