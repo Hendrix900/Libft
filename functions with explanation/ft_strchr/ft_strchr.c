85e7bd8b7cb8f6c07be55dfed2855cb23c403d19
ccastill <carlosc90@gmail.com>
2020-04-17 01:08:42 +0200

    1

--- functions with explanation/ft_strchr/ft_strchr.c
+++ functions with explanation/ft_strchr/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: carlos <carlos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 21:47:37 by ccastill          #+#    #+#             */
-/*   Updated: 2020/01/25 05:04:07 by carlos           ###   ########.fr       */
+/*   Updated: 2020/04/16 23:53:23 by carlos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strchr(const char *s, int c)
 	while (*s)
 	{
 		if (*s == c)
-			return ((char*)s); // La función devuelve un char * por lo que casteamos s.
+			return ((char*)s); // La función devuelve un char * por lo que casteamos s que es un const char.
 		++s;
 	}
 	if (c == '\0') // En caso de que llegue al final de la cadena, que devuelva toda la cadena.
